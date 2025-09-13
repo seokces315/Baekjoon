427595cd903fa1a375774edafdca838a78e8d27d
seokc <cshjames315@gmail.com>
2025-09-13 13:08:32 +0900

    Time Complexity

--- Time_Complexity/time_complexity_7_24313.py
+++ Time_Complexity/time_complexity_7_24313.py
@@ -3,6 +3,16 @@ a_1, a_0 = list(map(int, input().split()))
 c = int(input())
 n_0 = int(input())
 
+
 # Given function
 def f(n):
-    return a_1
+    return a_1 * n + a_0
+
+
+def g(n):
+    return n
+
+
+result = 1 if (f(n_0) <= c * g(n_0)) and (a_1 <= c) else 0
+
+print(result)
