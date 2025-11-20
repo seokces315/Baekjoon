ce1cc43b500d623807f9b186e4de7c8a08b19ddf
seokc <cshjames315@gmail.com>
2025-11-20 11:05:39 +0900

    Bruteforce done!

--- BruteForce/movie_director_1436.py
+++ BruteForce/movie_director_1436.py
@@ -1,2 +1,15 @@
 # Input processing
 N = int(input())  # 1 <= N <= 10,000
+
+# Solution loop
+cnt = 0
+cur_num = 665
+while cnt < N:  # Exit condition
+    # Move to the next number
+    cur_num += 1
+
+    # Check if the number contains the substring "666"
+    if "666" in str(cur_num):
+        cnt += 1
+
+print(cur_num)
